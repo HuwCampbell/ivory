bc64466fc7dcd9f6714d7174a9910955abd7a750
Charles O'Farrell <charleso@charleso.org>
2014-07-02 15:45:40 +1000

    Add IdentifierStorage for common Identifier file operations

---
+++ ivory-storage/src/main/scala/com/ambiata/ivory/storage/dictionary/DictionaryVersion.scala
@@ -0,0 +1,11 @@
+package com.ambiata.ivory.storage.dictionary
+
+sealed abstract class DictionaryVersion(override val toString: String)
+case object DictionaryVersionOne extends DictionaryVersion("1")
+
+object DictionaryVersion {
+  def fromString(str: String): Option[DictionaryVersion] = str match {
+    case "1" => Some(DictionaryVersionOne)
+    case _   => None
+  }
+}
