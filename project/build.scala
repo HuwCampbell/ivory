HuwCampbell/ivory

commit 0fa22ddbb587be1f350cd34e895d859c2f0684c7

Add explicit setting to ensure main is never set for assembly.

project/build.scala
@@ -79,6 +79,7 @@ object build extends Build {
   , settings = standardSettings ++ app("cli") ++ universalSettings ++ Seq[Settings](
       name := "ivory-cli"
     , dist
+    , mainClass in assembly := None
     ) ++ Seq[Settings](libraryDependencies ++= depend.scopt ++ depend.scalaz ++ depend.scoobi(version.value))
       ++ addArtifact(Artifact("ivory", "dist", "tgz"), packageZipTarball in Universal)
       ++ addArtifact(Artifact("ivory", "dist", "zip"), packageBin in Universal)

commit 1f92e31c45c79d0d354fb31d235f408a3559f03d

Remove hard-coded use of main class in ivory script
This was broken in 0fa22ddbb587be1f350cd34e895d859c2f0684c7
because setting mainClass in assembly is not enough.
It's better to specify this directly in the build anyway.

http://www.roman10.net/specify-the-main-class-in-a-jar-file-for-hadoop/

> If a jar file contains a main class specified in its manifest file,
> hadoop will take the main class even if the command specify another main class.

ivory-cli/src/main/bin/ivory
@@ -4,4 +4,4 @@
 BASE=$((readlink $0 || printf $0) | xargs dirname)/..
 JAR="${BASE}/lib/ivory.jar"
 
-exec hadoop jar "${JAR}" com.ambiata.ivory.cli.main "$@"
+exec hadoop jar "${JAR}" "$@"

project/build.scala
@@ -82,7 +82,7 @@ object build extends Build {
   , settings = standardSettings ++ app("cli") ++ universalSettings ++ Seq[Settings](
       name := "ivory-cli"
     , dist
-    , mainClass in assembly := None
+    , mainClass in assembly := Some("com.ambiata.ivory.cli.main")
     ) ++ Seq[Settings](libraryDependencies ++= depend.scopt ++ depend.scalaz ++ depend.scoobi(version.value))
       ++ addArtifact(Artifact("ivory", "dist", "tgz"), packageZipTarball in Universal)
       ++ addArtifact(Artifact("ivory", "dist", "zip"), packageBin in Universal)