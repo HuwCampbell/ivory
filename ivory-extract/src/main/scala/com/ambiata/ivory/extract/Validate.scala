2274e0eda66ca110d67ba4d52762ae2a5f7f54ff
Charles O'Farrell <charleso@charleso.org>
2014-07-15 09:27:56 +1000

    Simplify Validate.validateEncoding

--- ivory-extract/src/main/scala/com/ambiata/ivory/extract/Validate.scala
+++ ivory-extract/src/main/scala/com/ambiata/ivory/extract/Validate.scala
@@ -8,13 +8,9 @@ object Validate {
     dict.meta.get(fact.featureId).map(fm => validateEncoding(fact, fm.encoding)).getOrElse(s"Dictionary entry '${fact.featureId}' doesn't exist!".failure)
 
   def validateEncoding(fact: Fact, encoding: Encoding): Validation[String, Fact] = {
-    val v = fact.value
-    (encoding match {
-      case BooleanEncoding => if(v.encoding.exists(_ != BooleanEncoding)) s"Not a valid boolean! '${fact.toString}'".failure else Success(())
-      case IntEncoding     => if(v.encoding.exists(_ != IntEncoding)) s"Not a valid int! '${fact.toString}'".failure else Success(())
-      case LongEncoding    => if(v.encoding.exists(_ != LongEncoding)) s"Not a valid long! '${fact.toString}'".failure else Success(())
-      case DoubleEncoding  => if(v.encoding.exists(_ != DoubleEncoding)) s"Not a valid double! '${fact.toString}'".failure else Success(())
-      case StringEncoding  => if(v.encoding.exists(_ != StringEncoding)) s"Not a valid string! '${fact.toString}'".failure else Success(())
-    }).map(_ => fact)
+    import com.ambiata.ivory.storage.legacy.DictionaryTextStorage.encodingString
+    if (fact.value.encoding.exists(_ != encoding))
+      s"Not a valid ${encodingString(encoding)}! '${fact.toString}'".failure
+    else Success(fact)
   }
 }
