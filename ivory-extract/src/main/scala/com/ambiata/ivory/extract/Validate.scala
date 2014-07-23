fe79fc6cd2d34ed255d756f2b02f194b7efe7ea8
Charles O'Farrell <charleso@charleso.org>
2014-07-23 13:27:21 +1000

    Simply validateEncoding for just Values

--- ivory-extract/src/main/scala/com/ambiata/ivory/extract/Validate.scala
+++ ivory-extract/src/main/scala/com/ambiata/ivory/extract/Validate.scala
@@ -1,15 +1,16 @@
 package com.ambiata.ivory.extract
 
-import scalaz._, Scalaz._
+import scalaz.{Value => _, _}, Scalaz._
 import com.ambiata.ivory.core._
 
 object Validate {
   def validateFact(fact: Fact, dict: Dictionary): Validation[String, Fact] =
-    dict.meta.get(fact.featureId).map(fm => validateEncoding(fact, fm.encoding)).getOrElse(s"Dictionary entry '${fact.featureId}' doesn't exist!".failure)
+    dict.meta.get(fact.featureId)
+      .map {fm => validateEncoding(fact.value, fm.encoding).map(_ => fact).leftMap(_ + s" '${fact.toString}'")}
+      .getOrElse(s"Dictionary entry '${fact.featureId}' doesn't exist!".failure)
 
-  def validateEncoding(fact: Fact, encoding: Encoding): Validation[String, Fact] = {
-    if (fact.value.encoding.exists(_ != encoding))
-      s"Not a valid ${Encoding.render(encoding)}! '${fact.toString}'".failure
-    else Success(fact)
-  }
+  def validateEncoding(value: Value, encoding: Encoding): Validation[String, Unit] =
+    if (value.encoding.exists(_ != encoding))
+      s"Not a valid ${Encoding.render(encoding)}!".failure
+    else Success(())
 }
