ca23c6e8584b1bbbea6f75d5504487bbdbfff8e3
Charles O'Farrell <charleso@charleso.org>
2014-07-31 13:11:25 +1000

    Remove unused cluster KeyName
    May be needed for ssh tunneling in the future, but not now.

--- ivory-performance/src/test/bin/emr-start.sh
+++ ivory-performance/src/test/bin/emr-start.sh
@@ -2,7 +2,7 @@ export AWS_DEFAULT_REGION=ap-southeast-2
 
 aws configure set preview.emr true
 
-EMR_CLUSTER_JSON=$(aws emr create-cluster --ami-version 3.0.1 --no-auto-terminate --ec2-attributes KeyName=${EC2_KEY_PAIR} --tags Name=IvoryPerf --instance-groups \
+EMR_CLUSTER_JSON=$(aws emr create-cluster --ami-version 3.0.1 --no-auto-terminate --tags Name=IvoryPerf --instance-groups \
   InstanceGroupType=MASTER,InstanceCount=1,InstanceType=m1.large,BidPrice=0.05 \
   InstanceGroupType=CORE,InstanceCount=10,InstanceType=m1.large,BidPrice=0.05)
 export EMR_CLUSTER_ID=$(echo ${EMR_CLUSTER_JSON} | grep ClusterId | sed 's/.*\"\(.*\)\".*/\1/')
