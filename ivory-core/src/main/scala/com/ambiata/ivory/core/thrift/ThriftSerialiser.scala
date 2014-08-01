61673fc06acaa9e2e08028e7aa2a2a870ae83891
Charles O'Farrell <charleso@charleso.org>
2014-08-01 11:17:28 +1000

    Introduce ThriftSerialiser.fromBytesView for future potential of not copying arrays

--- ivory-core/src/main/scala/com/ambiata/ivory/core/thrift/ThriftSerialiser.scala
+++ ivory-core/src/main/scala/com/ambiata/ivory/core/thrift/ThriftSerialiser.scala
@@ -10,17 +10,26 @@ case class ThriftSerialiser() {
   def toBytes[A](a: A)(implicit ev: A <:< ThriftLike): Array[Byte] =
     serialiser.serialize(ev(a))
 
-  def fromBytes[A](empty: A, bytes: Array[Byte])(implicit ev: A <:< ThriftLike): A = {
-    val e = ev(empty).deepCopy
-    e.clear
+  def fromBytes[A](empty: A, bytes: Array[Byte])(implicit ev: A <:< ThriftLike): A =
+    fromBytesUnsafe(ev(empty).deepCopy.asInstanceOf[A], bytes)
+
+  def fromBytes1[A](empty: () => A, bytes: Array[Byte])(implicit ev: A <:< ThriftLike): A =
+    fromBytesUnsafe(empty(), bytes)
+
+  /* WARNING: This mutates the thrift value _in place_ - use with care and only for performance */
+  def fromBytesUnsafe[A](empty: A, bytes: Array[Byte])(implicit ev: A <:< ThriftLike): A = {
+    val e = ev(empty)
+    e.clear()
     deserialiser.deserialize(e, bytes)
     e.asInstanceOf[A]
   }
 
-  def fromBytes1[A](empty: () => A, bytes: Array[Byte])(implicit ev: A <:< ThriftLike): A = {
-    val e = empty()
-    deserialiser.deserialize(e, bytes)
-    e.asInstanceOf[A]
+  def fromBytesViewUnsafe[A](empty: A, bytes: Array[Byte], offset: Int, length: Int)(implicit ev: A <:< ThriftLike): A = {
+    fromBytesUnsafe(empty, if (offset == 0 && bytes.length == length) bytes else {
+      val result = new Array[Byte](length)
+      // TODO We need our own version of TDeserializer that uses the offset/length correctly
+      System.arraycopy(bytes, offset, result, 0, length)
+      result
+    })
   }
-
 }
