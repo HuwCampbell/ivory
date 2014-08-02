b9d942b86e1c695d7cd98c3bcc4e1855426d5814
Charles O'Farrell <charleso@charleso.org>
2014-08-02 12:35:18 +1000

    Introduce extra deserialize method onto TDeserializerCopy to avoid extra array copy

--- ivory-core/src/main/scala/com/ambiata/ivory/core/thrift/ThriftSerialiser.scala
+++ ivory-core/src/main/scala/com/ambiata/ivory/core/thrift/ThriftSerialiser.scala
@@ -1,11 +1,11 @@
 package com.ambiata.ivory.core.thrift
 
 import org.apache.thrift.protocol.TCompactProtocol
-import org.apache.thrift.{TSerializer, TDeserializer}
+import org.apache.thrift.TSerializer
 
 case class ThriftSerialiser() {
   val serialiser = new TSerializer(new TCompactProtocol.Factory)
-  val deserialiser = new TDeserializer(new TCompactProtocol.Factory)
+  val deserialiser = new TDeserializerCopy(new TCompactProtocol.Factory)
 
   def toBytes[A](a: A)(implicit ev: A <:< ThriftLike): Array[Byte] =
     serialiser.serialize(ev(a))
@@ -18,18 +18,13 @@ case class ThriftSerialiser() {
 
   /* WARNING: This mutates the thrift value _in place_ - use with care and only for performance */
   def fromBytesUnsafe[A](empty: A, bytes: Array[Byte])(implicit ev: A <:< ThriftLike): A = {
-    val e = ev(empty)
-    e.clear()
-    deserialiser.deserialize(e, bytes)
-    e.asInstanceOf[A]
+    fromBytesViewUnsafe(empty, bytes, 0, bytes.length)
   }
 
   def fromBytesViewUnsafe[A](empty: A, bytes: Array[Byte], offset: Int, length: Int)(implicit ev: A <:< ThriftLike): A = {
-    fromBytesUnsafe(empty, if (offset == 0 && bytes.length == length) bytes else {
-      val result = new Array[Byte](length)
-      // TODO We need our own version of TDeserializer that uses the offset/length correctly
-      System.arraycopy(bytes, offset, result, 0, length)
-      result
-    })
+    val e = ev(empty)
+    e.clear()
+    deserialiser.deserialize(e, bytes, offset, length)
+    e.asInstanceOf[A]
   }
 }
