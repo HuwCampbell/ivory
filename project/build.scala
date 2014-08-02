20c855d0f53f5268ec13da041084174547a2e89a
Charles O'Farrell <charleso@charleso.org>
2014-08-02 11:46:17 +1000

    Update SBT and enable consolidated resolution for update speedup
    https://github.com/sbt/sbt/issues/413

--- project/build.scala
+++ project/build.scala
@@ -53,6 +53,7 @@ object build extends Build {
   , publishArtifact in packageDoc := false
   // https://gist.github.com/djspiewak/976cd8ac65e20e136f05
   , unmanagedSourceDirectories in Compile += (sourceDirectory in Compile).value / s"scala-${scalaBinaryVersion.value}"
+  , updateOptions := updateOptions.value.withConsolidatedResolution(true)
   ) ++ Seq(prompt)
 
   def lib(name: String) =
