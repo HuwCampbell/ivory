554cf2fde252d071da2b81e644e5afcd9fcf644f
Charles O'Farrell <charleso@charleso.org>
2014-08-02 13:08:26 +1000

    Return Vector from Maps.outerJoin for micro optimisation
    https://gist.github.com/etorreborre/b7f2b0d7d149b5ec3bab

--- ivory-core/src/main/scala/com/ambiata/ivory/core/Maps.scala
+++ ivory-core/src/main/scala/com/ambiata/ivory/core/Maps.scala
@@ -4,10 +4,14 @@ import scalaz._, Scalaz._
 
 object Maps {
 
-  def outerJoin[K, V1, V2](m1: Map[K, V1], m2: Map[K, V2]): Map[K, \&/[V1, V2]] =
-    m1.map {
-      case (k, v) => k -> m2.get(k).cata(\&/.Both(v, _), \&/.This[V1, V2](v))
-    } ++ m2.flatMap {
-      case (k, v) => m1.get(k).cata(_ => None, Some(k -> \&/.That[V1, V2](v)))
+  def outerJoin[K, V1, V2](m1: Map[K, V1], m2: Map[K, V2]): Vector[(K, \&/[V1, V2])] = {
+    val b = Vector.newBuilder[(K, \&/[V1, V2])]
+    m1.foreach {
+      case (k, v) => b += k -> m2.get(k).cata(\&/.Both(v, _), \&/.This[V1, V2](v))
     }
+    m2.foreach {
+      case (k, v) => if (!m1.contains(k)) b += k -> \&/.That[V1, V2](v)
+    }
+    b.result()
+  }
 }
