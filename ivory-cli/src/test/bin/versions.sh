2cbc50721e6edc898a232a27caf0b13c53dbed22
Charles O'Farrell <charleso@charleso.org>
2014-08-05 15:47:30 +1000

    Added regression test for thrift + structs

--- ivory-cli/src/test/bin/versions.sh
+++ ivory-cli/src/test/bin/versions.sh
@@ -10,3 +10,7 @@ export IVORY_V0="1.0.0-cdh5-20140713234709-4656823 2.10"
 # Dictionary identifier names
 # Dictionary thrift format
 export IVORY_V1="1.0.0-cdh5-20140709064501-62fc154 2.10"
+
+# Structs
+# Thrift ingest
+export IVORY_V2="1.0.0-cdh5-20140805003023-b503696 2.11"
