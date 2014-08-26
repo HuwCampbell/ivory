eb772c0149c068c4a2ae0252b405d5dffe1f6bec
Charles O'Farrell <charleso@charleso.org>
2014-08-27 08:48:14 +1000

    Grammar in Arbitraries

--- ivory-core/src/test/scala/com/ambiata/ivory/core/Arbitraries.scala
+++ ivory-core/src/test/scala/com/ambiata/ivory/core/Arbitraries.scala
@@ -147,7 +147,7 @@ object Arbitraries {
     Arbitrary(for {
       n <- Gen.choose(10, 20)
       c <- Gen.listOfN(n, arbitrary[(FeatureId, ConcreteDefinition)])
-      // For every concrete definition there is a change we may have a virtual feature
+      // For every concrete definition there is a chance we may have a virtual feature
       v <- c.traverse(virtualDefGen).map(_.flatten)
     } yield Dictionary(c.map({ case (f, d) => d.toDefinition(f) }) ++ v.map({ case (f, d) => d.toDefinition(f) })))
 
