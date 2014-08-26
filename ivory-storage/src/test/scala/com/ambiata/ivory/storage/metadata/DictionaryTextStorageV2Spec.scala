a1e51ac65c9fee56606853653ce61fc163e42772
Charles O'Farrell <charleso@charleso.org>
2014-08-27 09:09:53 +1000

    Remove DictionaryTextStorageV2 sanity check test

--- ivory-storage/src/test/scala/com/ambiata/ivory/storage/metadata/DictionaryTextStorageV2Spec.scala
+++ ivory-storage/src/test/scala/com/ambiata/ivory/storage/metadata/DictionaryTextStorageV2Spec.scala
@@ -7,22 +7,11 @@ import scalaz.{Name => _, _}, Scalaz._
 
 class DictionaryTextStorageV2Spec extends Specification with ScalaCheck { def is = s2"""
   Parsing a dictionary on:
-    Sanity check                                $sanityCheck
     Invalid inputs will result in a failure     $invalidInput
     Parse any valid dictionary                  $anyDictionary
 """
 
   import DictionaryTextStorageV2._
-
-  def sanityCheck = {
-    val dict = Dictionary(List(
-      Definition.concrete(FeatureId(Name("a"), "b"), StringEncoding, Some(BinaryType), "", List("*")),
-      Definition.concrete(FeatureId("c", "d"), StructEncoding(Map("x" -> StructEncodedValue(BooleanEncoding))), None, "hello", Nil)
-    ))
-    fromString(
-      """a:b|encoding=string|type=binary|tombstone=*
-        |c:d|encoding=(x: boolean)|description=hello""".stripMargin) ==== dict.success
-  }
 
   def invalidInput = seqToResult(List(
     "invalid",
