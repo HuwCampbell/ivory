82e9d0b38bcb54bdf9ca5d95c23d308fcfc4a2e6
Maxwell Swadling <maxwell.swadling@nicta.com.au>
2014-08-28 11:53:13 +1000

    Added data syntax

--- ivory-data/src/main/scala/com/ambiata/ivory/data/StoreDataUtil.scala
+++ ivory-data/src/main/scala/com/ambiata/ivory/data/StoreDataUtil.scala
@@ -1,5 +1,6 @@
 package com.ambiata.ivory.data
 
+import DataSyntax._
 import com.ambiata.mundane.io._
 import com.ambiata.mundane.store._
 import scalaz.{Store => _, _}, Scalaz._
@@ -12,4 +13,7 @@ object StoreDataUtil {
     val path2 = if (path.path.startsWith("/")) path.path.substring(1).toFilePath else path
     store.list(path2).map(_.groupBy(_.relativeTo(path2).path.split("/").headOption).keys.flatten.map(path2 </> _).toList)
   }
+
+  def listDirWithoutDotFiles[F[+_] : Functor](store: Store[F], path: FilePath): F[List[FilePath]] =
+    listDir(store, path).map(_.filterHidden)
 }
