90e2cdeef9d1d5ab678bc09d3460ae72dc008c08
Eric Torreborre <etorreborre@yahoo.com>
2014-08-29 09:37:48 +1000

    fixed the EAVTImporter spec

--- ivory-operation/src/test/scala/com/ambiata/ivory/operation/ingestion/EavtTextImporterSpec.scala
+++ ivory-operation/src/test/scala/com/ambiata/ivory/operation/ingestion/EavtTextImporterSpec.scala
@@ -19,12 +19,12 @@ import org.joda.time.DateTimeZone
 import org.specs2._
 import org.specs2.execute.{AsResult, Result}
 import org.specs2.matcher.{FileMatchers, ThrownExpectations}
-import org.specs2.specification.Fixture
+import org.specs2.specification.{FixtureExample, Fixture}
 import com.ambiata.ivory.operation.ingestion.thrift._
 import scalaz.effect.IO
 import scalaz.{Name => _, DList => _, _}, Scalaz._
 
-class EavtTextImporterSpec extends Specification with ThrownExpectations with FileMatchers { def is = s2"""
+class EavtTextImporterSpec extends Specification with ThrownExpectations with FileMatchers with FixtureExample[Setup] { def is = s2"""
 
   MR job runs and creates expected text data $textData
 
@@ -34,21 +34,35 @@ class EavtTextImporterSpec extends Specification with ThrownExpectations with Fi
 
   """
 
-  def textData = setup { setup: Setup =>
+  def textData = { setup: Setup =>
     import setup._
 
     saveInputFile >>
     run(setup, TextFormat) must beOkValue(expected)
   }
 
-  def thriftData = setup { setup: Setup =>
-    import setup._
+  def thriftData = { setup: Setup =>
     val serializer = ThriftSerialiser()
+    import setup._
 
-    SequenceUtil.writeBytes(namespaced.toFilePath </> java.util.UUID.randomUUID().toString, None) {
+    SequenceUtil.writeBytes(directory </> "input" </> "ns1" </> java.util.UUID.randomUUID().toString, None) {
       writer => ResultT.safe(expected.map(Conversion.fact2thrift).map(fact => serializer.toBytes(fact)).foreach(writer))
     }.run(setup.sc) >>
     run(setup, ThriftFormat) must beOkValue(expected)
+  }
+
+  def errorMessage = { setup: Setup =>
+    import setup._
+    // save an input file containing errors
+    val errors = base </> "errors"
+
+    // run the scoobi job to import facts on Hdfs
+    (for {
+      _         <- saveInputFileWithErrors
+      errorPath <- Reference.hdfsPath(errors)
+      _         <- EavtTextImporter.onStore(repository, dictionary, FactsetId.initial, List(ns1), None, input, errors, DateTimeZone.getDefault, List(ns1 -> 1.mb), 128.mb, TextFormat)
+      ret = valueFromSequenceFile[ParseError](errorPath.toString).run
+    } yield ret) must beOkLike(_ must not(beEmpty))
   }
 
   def run(setup: Setup, format: Format) = {
@@ -57,29 +71,13 @@ class EavtTextImporterSpec extends Specification with ThrownExpectations with Fi
     val errors = base </> "errors"
     // run the scoobi job to import facts on Hdfs
     EavtTextImporter.onStore(repository, dictionary, FactsetId.initial, List(ns1), None, input, errors, DateTimeZone.getDefault, List(ns1 -> 1.mb), 128.mb, format) >>
-    factsFromIvoryFactset(repository, FactsetId.initial).map(_.run.collect { case \/-(r) => r }.toSet).run(sc)
+      factsFromIvoryFactset(repository, FactsetId.initial).map(_.run.collect { case \/-(r) => r }.toSet).run(sc)
   }
 
-  def errorMessage = setup { setup: Setup =>
-    import setup._
-    // save an input file containing errors
-    saveInputFileWithErrors
-    val errors = base </> "errors"
-
-    // run the scoobi job to import facts on Hdfs
-    (for {
-      errorPath <- Reference.hdfsPath(errors)
-      _         <- EavtTextImporter.onStore(repository, dictionary, FactsetId.initial, List(ns1), None, input, errors, DateTimeZone.getDefault, List(ns1 -> 1.mb), 128.mb, TextFormat)
-      ret = valueFromSequenceFile[ParseError](errorPath.toString).run
-    } yield ret) must beOkLike(_ must not(beEmpty))
-  }
-
-  def setup: Fixture[Setup] = new Fixture[Setup] {
-    def apply[R : AsResult](f: Setup => R): Result =
-      Temporary.using { dir =>
-        ResultT.ok[IO, Result](AsResult(f(new Setup(dir))))
-      } must beOkLike(r => r.isSuccess aka r.message must beTrue)
-  }
+  def fixture[R : AsResult](f: Setup => R): Result =
+    Temporary.using { dir =>
+       ResultT.ok[IO, Result](AsResult(f(new Setup(dir))))
+    } must beOkLike(r => r.isSuccess aka r.message must beTrue)
 
   // This needs to be a function otherwise Scoobi will serialise with xstream :(
   def expected = Set(
@@ -90,13 +88,13 @@ class EavtTextImporterSpec extends Specification with ThrownExpectations with Fi
   val ns1 = Name("ns1")
 }
 
-class Setup(directory: FilePath) {
+class Setup(val directory: FilePath) {
   implicit def sc: ScoobiConfiguration = TestConfigurations.scoobiConfiguration
   implicit lazy val fs = sc.fileSystem
 
   lazy val base = Reference(HdfsStore(sc, directory), FilePath.root)
   lazy val input = base </> "input"
-  lazy val namespaced = (directory </> "input" </> "ns1").path
+  lazy val namespaced = (input </> "ns1").path
   lazy val repository = Repository.fromHdfsPath(directory </> "repo", sc)
 
   val dictionary =
@@ -119,6 +117,6 @@ class Setup(directory: FilePath) {
     save(namespaced, raw)
   }
 
-  def save(path: String, raw: List[String]) = 
-    repository.toStore.linesUtf8.write(FilePath(path) </> "part", raw)
+  def save(path: FilePath, raw: List[String]) =
+    (base </> path </> "part").run(store => fp => store.linesUtf8.write(fp, raw))
 }
