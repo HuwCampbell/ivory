aaeb9c93fd3feb0c11c0ebff292590666faf9c28
Nick Hibberd <nhibberd@gmail.com>
2014-08-28 11:40:05 +1000

    Importing dictionary from any reference to any type of ivory repository
    Adding generic test for loading dictionaries to different store types
    
    Adding fromStore to Repository
    
    Temporary testing util to create and cleanup a Reference / Store / Repository
    
    Adding StoreType Arbitrary
    
    Adding StoreType Arbitrary to DictionaryIMporterSpec
    
    Updating Temporary and adding test
    
    Cleaning up RepositoriesSpec
    
    Adding aws tag to s3 tests in TemporarySpec
    
    Updating Temporary
    
    Adding cross store dictionary import spec
    
    cleanup

--- ivory-storage/src/test/scala/com/ambiata/ivory/storage/repository/RepositoriesSpec.scala
+++ ivory-storage/src/test/scala/com/ambiata/ivory/storage/repository/RepositoriesSpec.scala
@@ -1,17 +1,14 @@
 package com.ambiata.ivory.storage.repository
 
-import java.util.UUID
-
-import com.ambiata.ivory.core.Crash
+import com.ambiata.ivory.storage.{Temporary => T}
 import com.ambiata.mundane.control.ResultTIO
-import com.ambiata.mundane.io.{FilePath, Temporary}
 import com.ambiata.mundane.testing.ResultTIOMatcher._
 import com.nicta.scoobi.impl.ScoobiConfiguration
-import org.specs2.matcher.ThrownExpectations
 import org.specs2.{ScalaCheck, Specification}
-import scalaz._, Scalaz._
 
-class RepositoriesSpec extends Specification with ScalaCheck with ThrownExpectations { def is = s2"""
+import scalaz.Scalaz._
+
+class RepositoriesSpec extends Specification with ScalaCheck { def is = s2"""
 
 Create Repository Tests
 -----------------------
@@ -25,35 +22,23 @@ Create repository should always create all folders
 
   lazy val conf = RepositoryConfiguration(ScoobiConfiguration())
 
-  def hdfs = {
-    Temporary.using { tmp =>
-      val base = tmp </> "base"
-      val repo: Repository = Repository.fromUri(s"hdfs://${base}", conf).toOption.getOrElse(Crash.error(Crash.ResultTIO, "Can not create repository on hdfs"))
-      createRepository(repo)
-      checkRepository(repo)
+  def hdfs =
+    exists(T.Hdfs)
+
+  def s3 =
+    exists(T.S3)
+
+  def local =
+    exists(T.Posix)
+
+  def exists(repository: T.TemporaryType) = {
+    T.withRepository(repository) { repo =>
+      createRepository(repo) >> checkRepository(repo)
     } must beOkLike(_ must contain(true).forall)
   }
 
-  def s3 = {
-    val repo: Repository = Repository.fromUri(s"s3://ambiata-dev-view/tests/${UUID.randomUUID()}", conf).toOption.getOrElse(Crash.error(Crash.ResultTIO, "Can not create repository on s3"))
-    createRepository(repo)
-    checkRepository(repo) must beOkLike(_ must contain(true).forall)
-    repo.toStore.deleteAll(Repository.root) must beOk
-  }
-
-  def local = {
-    Temporary.using { tmp =>
-      val base = tmp </> "base"
-      val repo: Repository = Repository.fromUri(s"file://${base}", conf).toOption.getOrElse(Crash.error(Crash.ResultTIO, "Can not create repository on local file system"))
-      createRepository(repo)
-      checkRepository(repo)
-    } must beOkLike(_ must contain(true).forall)
-  }
-
-  def createRepository(repo: Repository) = {
-    Repositories.create(repo) must beOk
-    Repositories.create(repo) must beOk
-  }
+  def createRepository(repo: Repository) =
+    Repositories.create(repo) >> Repositories.create(repo)
 
   def checkRepository(repo: Repository): ResultTIO[List[Boolean]] = {
     List(
