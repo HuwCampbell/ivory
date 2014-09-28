33469bcef2173572b4869e115c42afc631c04e55
Charles O'Farrell <charleso@charleso.org>
2014-09-29 09:57:39 +1000

    Switch ivory-cli to use hadoop-dev for now

--- ivory-cli/src/test/bin/build.sh
+++ ivory-cli/src/test/bin/build.sh
@@ -1,12 +1,12 @@
 IVORY=${IVORY:-}
 
 # Only build once, or if you want to override it for testing
-if [ ! -e "$IVORY" ]; then
+if [ ! -n "$IVORY" ]; then
     echo "Building ivory-cli"
 
     TARGET="${PROJECT}/target/ivory-cli"
     mkdir -p "${TARGET}"
     ${PROJECT}/sbt -Dsbt.log.noformat=true ";project cli; clean; universal:package-zip-tarball"
     tar xvf ${PROJECT}/ivory-cli/target/universal/ivory-cli*.tgz --strip-components 1 -C ${TARGET}
-    export IVORY=${TARGET}/bin/ivory
+    export IVORY="${PROJECT}/bin/hadoop-dev ${TARGET}/lib/ivory.jar com.ambiata.ivory.cli.main"
 fi
