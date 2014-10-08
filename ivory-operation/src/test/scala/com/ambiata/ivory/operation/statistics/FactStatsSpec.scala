d7d845cff6b4068235b62f1da3dc6bbd1d5a99d6
Huw Campbell <huw.campbell@ambiata.com>
2014-10-08 15:38:04 +1100

    Add statistics cli app
    Turn overwrite to false

---
+++ ivory-operation/src/test/scala/com/ambiata/ivory/operation/statistics/FactStatsSpec.scala
@@ -0,0 +1,57 @@
+package com.ambiata.ivory.operation.diff
+
+import com.ambiata.mundane.control.ResultT
+import com.nicta.scoobi.testing.TestFiles
+import org.specs2._
+import org.specs2.execute.{Result, AsResult}
+import org.specs2.matcher.{ThrownExpectations, FileMatchers}
+import com.ambiata.ivory.core._
+
+import scalaz.effect.IO
+import argonaut._, Argonaut._
+
+class FactStatsSpec extends Specification with ThrownExpectations with FileMatchers { def is = sequential ^ s2"""
+
+  FactStats can get the histogram and numerical statistics for of a set of facts      $e1
+  FactStats can be written to and from JSON                                           $e2
+
+  """
+
+  // Note: On merge with snapshot meta branch, these should be deleted.
+  implicit def DateJsonCodec: CodecJson[Date] = CodecJson.derived(
+    EncodeJson(_.int.asJson),
+    DecodeJson.optionDecoder(_.as[Int].toOption.flatMap(Date.fromInt), "Date"))
+
+  type KeyInfo        = (String, Date)
+  type Histogram      = Map[String,Int]
+  type NumericalStats = (Long, Double, Double)
+  type FactStatEncode = (KeyInfo, Either[NumericalStats, Histogram])
+
+  val dictionary = Dictionary(List(Definition.concrete(FeatureId(Name("ns1"), "fid1"), StringEncoding, Some(CategoricalType), "", Nil)
+                                  ,Definition.concrete(FeatureId(Name("ns1"), "fid2"), IntEncoding, Some(NumericalType), "", Nil)))
+ 
+  val concreteTypes = dictionary.byConcrete.sources.mapValues(cd => cd.definition.ty)
+
+  val facts1 = Seq(StringFact("eid1",  FeatureId(Name("ns1"), "fid1"), Date(2012, 10, 1), Time(0), "horus"),
+                   StringFact("eid2",  FeatureId(Name("ns1"), "fid1"), Date(2012, 10, 1), Time(0), "horus"),
+                   StringFact("eid3",  FeatureId(Name("ns1"), "fid1"), Date(2012, 10, 1), Time(0), "anubis"),
+                   StringFact("eid4",  FeatureId(Name("ns1"), "fid1"), Date(2012, 10, 1), Time(0), "anubis"),
+                   StringFact("eid4",  FeatureId(Name("ns1"), "fid1"), Date(2012, 10, 1), Time(0), "anubis"),
+                   StringFact("eid5",  FeatureId(Name("ns1"), "fid1"), Date(2012, 10, 1), Time(0), "isis")
+                  )
+  val facts2 = Seq(IntFact("eid1",     FeatureId(Name("ns1"), "fid2"), Date(2012, 10, 2), Time(0), 1),
+                   IntFact("eid3",     FeatureId(Name("ns1"), "fid2"), Date(2012, 10, 2), Time(0), 9)
+               )
+
+  def e1 = {
+      FactStats.genStats(concreteTypes, (FeatureId(Name("ns1"), "fid1"), Date(2012, 10, 1)), facts1.toIterable) ==== List(((FeatureId(Name("ns1"), "fid1").toString, Date(2012, 10, 1)), Right(Map("horus" -> 2, "anubis" -> 3, "isis" -> 1))))
+      FactStats.genStats(concreteTypes, (FeatureId(Name("ns1"), "fid2"), Date(2012, 10, 2)), facts2.toIterable) ==== List(((FeatureId(Name("ns1"), "fid2").toString, Date(2012, 10, 2)), Right(Map("1" -> 1, "9" -> 1))),
+                                                                                                                          ((FeatureId(Name("ns1"), "fid2").toString, Date(2012, 10, 2)), Left( (2, 5.0, 4.0))))
+  }
+
+  def e2 = {
+    val f = FactStats.genStats(concreteTypes, (FeatureId(Name("ns1"), "fid2"), Date(2012, 10, 2)), facts2.toIterable)
+    println(f.asJson.nospaces)
+    Parse.decodeOption[List[FactStatEncode]](f.asJson.nospaces) must beSome(f)
+  }
+}
