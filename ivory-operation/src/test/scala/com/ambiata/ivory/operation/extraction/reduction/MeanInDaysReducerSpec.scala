ecc5625ce462f7c61ea7f621033a2a399fa3a948
Charles O'Farrell <charleso@charleso.org>
2014-10-13 16:58:00 +1100

    Fix MeanInDaysReducerSpec for edge cases

--- ivory-operation/src/test/scala/com/ambiata/ivory/operation/extraction/reduction/MeanInDaysReducerSpec.scala
+++ ivory-operation/src/test/scala/com/ambiata/ivory/operation/extraction/reduction/MeanInDaysReducerSpec.scala
@@ -1,5 +1,6 @@
 package com.ambiata.ivory.operation.extraction.reduction
 
+import com.ambiata.ivory.core.Date
 import com.ambiata.ivory.operation.extraction.reduction.ReductionArbitraries._
 import org.specs2.{ScalaCheck, Specification}
 
@@ -13,7 +14,9 @@ class MeanInDaysReducerSpec extends Specification with ScalaCheck { def is = s2"
   })
 
   def meanInWeeks = prop((doc: DatesOfCount) => {
+    // We ignore the days not contained in a full week
+    val start = doc.dates.lastOption.map(_._2).map(_.localDate.minusDays(doc.noOfWeeksFloor * 7)).map(Date.fromLocalDate).getOrElse(Date.minValue)
     ReducerUtil.reduceDates(doc, new MeanInWeeksReducer) ====
-      (if (doc.noOfWeeksFloor != 0) doc.dates.map(_._1).sum / doc.noOfWeeksFloor else 0)
+      (if (doc.noOfWeeksFloor != 0) doc.dates.filter(_._2.int >= start.int).map(_._1).sum / doc.noOfWeeksFloor else 0)
   })
 }
