5bb4c8524b590d6c421c18404628d3b572d7da49
Dom De Re <domdere@domdere.com>
2014-10-13 15:08:44 +1100

    minor renaming

--- ivory-storage/src/main/scala/com/ambiata/ivory/storage/metadata/SnapshotMetadata.scala
+++ ivory-storage/src/main/scala/com/ambiata/ivory/storage/metadata/SnapshotMetadata.scala
@@ -11,9 +11,27 @@ import argonaut._, Argonaut._
 import com.ambiata.mundane.control._
 import com.ambiata.mundane.store._
 
-sealed trait SnapshotMeta
-case class SnapshotMetaLegacy(legacyMeta: legacy.SnapshotMeta) extends SnapshotMeta
-case class SnapshotMetaJSON(jsonMeta: JSONSnapshotMeta) extends SnapshotMeta
+sealed trait SnapshotMetadata
+{
+  def snapshotId(x: SnapshotMetadata): SnapshotId = this match {
+    case SnapshotMetaLegacy(lm) => {
+      lm.snapshotId
+    }
+    case SnapshotMetaJSON(jm) => jm.snapshotId
+  }
+}
+
+case class SnapshotMetaLegacy(legacyMeta: legacy.SnapshotMeta) extends SnapshotMetadata
+case class SnapshotMetaJSON(jsonMeta: JSONSnapshotMeta) extends SnapshotMetadata
+
+object SnapshotMetadata
+{
+  def snapshotMetaLegacy(legacyMeta: legacy.SnapshotMeta): SnapshotMetadata =
+    new SnapshotMetaLegacy(legacyMeta)
+
+  def snapshotMetaJSON(jsonMeta: JSONSnapshotMeta): SnapshotMetadata =
+    new SnapshotMetaJSON(jsonMeta)
+}
 
 // NOTE (Dom): formatting?
 case class JSONSnapshotMeta(
@@ -26,7 +44,7 @@ case class JSONSnapshotMeta(
   // NOTE (Dom): I'm guessing this is used to figure out the latest snapshot.
   // This seems "OK" right now since it doesnt seem to be used for much else.
   // But in time after more and more stuff gets added to it, will it still be appropriate
-  // to be encoding this into the "semantics" of the metadata with Order[SnapshotMeta]?
+  // to be encoding this into the "semantics" of the metadata with Order[JSONSnapshotMeta]?
   //
   def order(other: JSONSnapshotMeta): Ordering =
     (snapshotId, date, commitId).?|?((other.snapshotId, other.date, other.commitId))
