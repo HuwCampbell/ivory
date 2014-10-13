eacd64c444bfdac11ef55afd64e46ef5f15b40c9
Dom De Re <Dom.DeRe@nicta.com.au>
2014-10-13 11:55:31 +1100

    Proper CodecJson instance for Date

--- ivory-core/src/main/scala/com/ambiata/ivory/core/Date.scala
+++ ivory-core/src/main/scala/com/ambiata/ivory/core/Date.scala
@@ -134,6 +134,14 @@ object Date {
   implicit def DateOrdering =
     DateOrder.toScalaOrdering
 
+  // NOTE (Dom): Could this be cleaner?
+  implicit def DataJsonCodec: CodecJson[Date] = CodecJson(
+    (_.string("-").asJson),
+    ((c : HCursor) => c.as[String].flatMap((s : String) => listParser.run(s.pure[List]) match {
+      case Success(x)   => x.pure[DecodeResult]
+      case Failure(err) => DecodeResult.fail("Failed to parse Date: " + err, c.history)
+    })))
+
   /**
    * This is not epoch! It will take a long which was created from Date.addSeconds and
    * pull the original Date and seconds out.
