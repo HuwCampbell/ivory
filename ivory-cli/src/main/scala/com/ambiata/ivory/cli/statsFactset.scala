4c7696f963316ba4efe797e43f3a816b33a169d8
Charles O'Farrell <charleso@charleso.org>
2014-10-20 17:18:22 +1100

    Use IvoryCmd.withRepo for statsFactset

--- ivory-cli/src/main/scala/com/ambiata/ivory/cli/statsFactset.scala
+++ ivory-cli/src/main/scala/com/ambiata/ivory/cli/statsFactset.scala
@@ -9,25 +9,23 @@ import scalaz.effect._
 
 object statsFactset extends IvoryApp {
   case class CliArguments(
-    repo:    String,
     factSet: String
   )
 
   val parser = new scopt.OptionParser[CliArguments]("factset-statistics") {
     head("""
-           | Calculate and store stastics of features in a fact-set
+           | Calculate and store statistics of features in a fact-set
            |""".stripMargin)
 
     help("help") text "shows this usage text"
 
-    opt[String]('r', "repository") action { (x, c) => c.copy(repo = x) }     required() text "Input ivory repository."
     opt[String]('f', "fact-set")   action { (x, c) => c.copy(factSet = x) }  required() text "Input ivory factset ID."
   }
 
-  val cmd = new IvoryCmd[CliArguments](parser, CliArguments("", ""), IvoryRunner { configuration => c =>
+  val cmd = IvoryCmd.withRepo[CliArguments](parser, CliArguments(""), { repo => configuration => c =>
 
     for {
-      repo          <- HdfsRepository.fromUri(c.repo, configuration)
+      repo          <- HdfsRepository.fromUri(repo.root.show, configuration)
       factsetId     <- ResultT.fromOption[IO, FactsetId](FactsetId.parse(c.factSet), s"Could not parse FactsetId ${c.factSet}")
       res            = IvoryRetire.statsFacts(repo, factsetId)
       _             <- res.run(configuration.scoobiConfiguration)
