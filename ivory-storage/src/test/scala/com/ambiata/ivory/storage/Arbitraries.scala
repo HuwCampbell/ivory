0e30468bbfbd937f190ef3ab1fddda5c329f263a
Dom De Re <domdere@domdere.com>
2014-10-21 17:37:54 +1100

    Added arbitraries for SnapshotManifest

--- ivory-storage/src/test/scala/com/ambiata/ivory/storage/Arbitraries.scala
+++ ivory-storage/src/test/scala/com/ambiata/ivory/storage/Arbitraries.scala
@@ -3,6 +3,9 @@ package com.ambiata.ivory.storage
 import com.ambiata.ivory.core._
 import com.ambiata.ivory.data.Identifier
 import com.ambiata.ivory.storage.fact._
+import com.ambiata.ivory.storage.metadata._
+import com.ambiata.ivory.storage.legacy._
+
 import com.ambiata.ivory.core.Arbitraries._
 import com.ambiata.ivory.data.Arbitraries._
 import com.ambiata.notion.core._
@@ -27,4 +30,24 @@ object Arbitraries {
 
   implicit def StoreTypeArbitrary: Arbitrary[TemporaryType] =
     Arbitrary(Gen.oneOf(Posix, S3, Hdfs))
+
+  implicit def NewSnapshotManifestArbitrary: Arbitrary[NewSnapshotManifest] =
+    Arbitrary(for {
+      id <- arbitrary[SnapshotId]
+      date <- arbitrary[Date]
+      commitId <- arbitrary[CommitId]
+    } yield NewSnapshotManifest.newSnapshotMeta(id, date, commitId))
+
+  implicit def SnapshotMetaArbitrary: Arbitrary[SnapshotMeta] =
+    Arbitrary(for {
+      id <- arbitrary[SnapshotId]
+      date <- arbitrary[Date]
+      featureStoreId <- arbitrary[FeatureStoreId]
+      commitId <- arbitrary[Option[CommitId]]
+    } yield SnapshotMeta(id, date, featureStoreId, commitId))
+
+  implicit def SnapshotManifestArbitrary: Arbitrary[SnapshotManifest] =
+    Arbitrary(Gen.oneOf(
+      arbitrary[SnapshotMeta].map(SnapshotManifest.snapshotManifestLegacy),
+      arbitrary[NewSnapshotManifest].map(SnapshotManifest.snapshotManifestNew)))
 }
