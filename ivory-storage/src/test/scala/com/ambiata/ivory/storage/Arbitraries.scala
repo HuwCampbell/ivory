23cc71d9c9796eb92dc7bc3094dfaaf051017a45
Dom De Re <domdere@domdere.com>
2014-10-23 15:21:29 +1100

    Arbitraries for a list of reasonable legacy snapshotmetas

--- ivory-storage/src/test/scala/com/ambiata/ivory/storage/Arbitraries.scala
+++ ivory-storage/src/test/scala/com/ambiata/ivory/storage/Arbitraries.scala
@@ -12,6 +12,9 @@ import com.ambiata.notion.core._
 import com.ambiata.notion.core.TemporaryType._
 import org.scalacheck._, Arbitrary._
 
+import scalaz._, Scalaz._
+import scalaz.scalacheck.ScalaCheckBinding._
+
 import plan._
 
 object Arbitraries {
@@ -37,6 +40,47 @@ object Arbitraries {
       date <- arbitrary[Date]
       commitId <- arbitrary[CommitId]
     } yield NewSnapshotManifest.newSnapshotMeta(id, date, commitId))
+
+  case class SnapshotMetaList(metas: List[SnapshotMeta])
+
+  implicit def SnapshotMetaListArbitrary: Arbitrary[SnapshotMetaList] = Arbitrary(
+    for {
+      ids <- arbitrary[SmallSnapshotIdList]
+      // Restricted to some simple cases...
+      metas <- Gen.oneOf(
+          genSnapshotMetas(ids)
+        , genSameDateSnapshotMetas(ids)
+        , genSameStoreSnapshotMetas(ids)
+        , genSameSnapshotMetas(ids))
+    } yield SnapshotMetaList(metas))
+
+  def genSnapshotMetas(ids: SmallSnapshotIdList): Gen[List[SnapshotMeta]] =
+    ids.ids.traverseU((id: SnapshotId) =>
+      for {
+        date <- arbitrary[Date]
+        storeId <- arbitrary[FeatureStoreId]
+        // commitIds should be increasing with dates, but since we are testing ordering
+        // with these and the commitId is not used, this shouldnt affect the results.
+        commitId <- arbitrary[Option[CommitId]]
+      } yield SnapshotMeta(id, date, storeId, commitId))
+
+  def genSameDateSnapshotMetas(ids: SmallSnapshotIdList): Gen[List[SnapshotMeta]] = for {
+    date <- arbitrary[Date]
+    commitId <- arbitrary[Option[CommitId]]
+    metas <- ids.ids.traverseU((id: SnapshotId) => arbitrary[FeatureStoreId].map(SnapshotMeta(id, date, _, commitId)))
+  } yield metas
+
+  def genSameStoreSnapshotMetas(ids: SmallSnapshotIdList): Gen[List[SnapshotMeta]] = for {
+    storeId <- arbitrary[FeatureStoreId]
+    commitId <- arbitrary[Option[CommitId]]
+    metas <- ids.ids.traverseU((id: SnapshotId) => arbitrary[Date].map(SnapshotMeta(id, _, storeId, commitId)))
+  } yield metas
+
+  def genSameSnapshotMetas(ids: SmallSnapshotIdList): Gen[List[SnapshotMeta]] = for {
+    date <- arbitrary[Date]
+    store <- arbitrary[FeatureStoreId]
+    commitId <- arbitrary[Option[CommitId]]
+  } yield ids.ids.map(SnapshotMeta(_, date, store, commitId))
 
   implicit def SnapshotMetaArbitrary: Arbitrary[SnapshotMeta] =
     Arbitrary(for {
