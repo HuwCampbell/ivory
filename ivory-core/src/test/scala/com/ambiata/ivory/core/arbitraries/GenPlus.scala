18bcacaf1fa77f95450753b7812fb66d49ba9c2f
Charles O'Farrell <charleso@charleso.org>
2014-10-23 16:12:31 +1100

    Fix the chance of tests being discarded for MR property specs

---
+++ ivory-core/src/test/scala/com/ambiata/ivory/core/arbitraries/GenPlus.scala
@@ -0,0 +1,15 @@
+package com.ambiata.ivory.core.arbitraries
+
+import org.scalacheck.Gen
+import org.scalacheck.Gen.Choose
+
+object GenPlus {
+
+  // The Gen version of this function results in discarded tests
+  def nonEmptyListOf[A](gen: => Gen[A]): Gen[List[A]] =
+    Gen.sized(n => Gen.choose(1, Math.max(1, n)).flatMap(i => Gen.listOfN(i, gen)))
+
+  // The Gen version of this function resulted in discarded tests
+  def posNum[A](implicit num: Numeric[A], c: Choose[A]): Gen[A] =
+    Gen.sized(max => c.choose(num.one, num.max(num.one, num.fromInt(max))))
+}
