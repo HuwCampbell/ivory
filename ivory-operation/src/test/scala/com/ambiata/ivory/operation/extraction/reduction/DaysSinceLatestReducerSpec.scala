9c28f72c023e645faf8b9079957bd8b88a5a956d
Charles O'Farrell <charleso@charleso.org>
2014-10-31 22:37:57 +1100

    Added missing spec for days_since_latest

---
+++ ivory-operation/src/test/scala/com/ambiata/ivory/operation/extraction/reduction/DaysSinceLatestReducerSpec.scala
@@ -0,0 +1,22 @@
+package com.ambiata.ivory.operation.extraction.reduction
+
+import com.ambiata.ivory.core._, Arbitraries._
+import org.specs2.{ScalaCheck, Specification}
+
+class DaysSinceLatestReducerSpec extends Specification with ScalaCheck { def is = s2"""
+  Days since latest                                $daysSinceLatest
+"""
+
+  def daysSinceLatest = prop((fs: List[Fact]) => {
+    val facts = fs.sortBy(_.date)
+    val dateOffsets = DateOffsets.compact(
+      facts.headOption.map(_.date).getOrElse(Date.minValue),
+      facts.lastOption.map(_.date).getOrElse(Date.minValue)
+    )
+    val r = new DaysSinceLatestReducer(dateOffsets)
+    r.clear()
+    facts.foreach(r.update)
+    facts.lastOption.filterNot(_.isTombstone).map(f => dateOffsets.untilEnd(f.date).value)
+      .map(_ ==== r.save.getI).getOrElse(r.save must beNull)
+  }).set(minTestsOk = 10)
+}
