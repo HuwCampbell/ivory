231f3985c3a2708c51713da6d9e2878f0b03ed2b
Charles O'Farrell <charleso@charleso.org>
2014-11-02 10:32:36 +1100

    Improve CountByReducerSpec

--- ivory-operation/src/test/scala/com/ambiata/ivory/operation/extraction/reduction/CountByReducerSpec.scala
+++ ivory-operation/src/test/scala/com/ambiata/ivory/operation/extraction/reduction/CountByReducerSpec.scala
@@ -8,7 +8,9 @@ class CountByReducerSpec extends Specification with ScalaCheck { def is = s2"""
   Take the count of an arbitrary values                       $countBy
 """
 
-  def countBy = prop((xs: List[String]) =>
-    ReducerUtil.run(new CountByReducer[String], xs).map.asScala.toMap ==== xs.groupBy(identity).mapValues(_.length.toLong)
-  )
+  def countBy = prop((l: List[(String, Byte)]) => {
+    val xs = l.toMap
+    val xss = xs.toList.flatMap(x => (0 until Math.abs(x._2)).map(_ => x._1))
+    ReducerUtil.run(new CountByReducer[String], xss).map.asScala.toMap ==== xs.mapValues(Math.abs(_).toLong).filter(_._2 > 0)
+  })
 }
