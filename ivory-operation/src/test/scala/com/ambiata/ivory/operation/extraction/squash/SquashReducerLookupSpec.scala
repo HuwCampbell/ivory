a62747d502d9436633ccb5798722aae980e0ecf7
Charles O'Farrell <charleso@charleso.org>
2014-11-02 13:15:03 +1100

    Issue #391: Fix flakey Squash partitionEntity

--- ivory-operation/src/test/scala/com/ambiata/ivory/operation/extraction/squash/SquashReducerLookupSpec.scala
+++ ivory-operation/src/test/scala/com/ambiata/ivory/operation/extraction/squash/SquashReducerLookupSpec.scala
@@ -3,7 +3,7 @@ package com.ambiata.ivory.operation.extraction.squash
 import com.ambiata.ivory.core.Arbitraries._
 import com.ambiata.ivory.core._
 import com.ambiata.ivory.lookup.ReducerLookup
-import com.ambiata.poacher.mr.Writables
+import com.ambiata.poacher.mr.{ByteWriter, Writables}
 import org.specs2.{ScalaCheck, Specification}
 
 class SquashReducerLookupSpec extends Specification with ScalaCheck { def is = s2"""
@@ -33,15 +33,23 @@ class SquashReducerLookupSpec extends Specification with ScalaCheck { def is = s
     SquashPartitioner.getPartition(lookup, bw, partitions) ==== fid % partitions
   })
 
-  def partitionEntity = prop((f: Fact, fids: Short, e1: String, e2: String) => e1 != e2 ==> {
+  def partitionEntity = prop((f: Fact, fids: Short, e1: String, e2: String) => hashEntry(e1) != hashEntry(e2) ==> {
     val fid = Math.abs(fids).toShort
     val lookup = new ReducerLookup
     lookup.putToReducers(fid, FeatureReducerOffset(0, Short.MaxValue - 1).toInt)
-    val bw = Writables.bytesWritable(2048)
-    SquashWritable.KeyState.set(f.withEntity(e1), bw, fid)
-    val a = SquashPartitioner.getPartition(lookup, bw, Integer.MAX_VALUE)
-    SquashWritable.KeyState.set(f.withEntity(e2), bw, fid)
-    val b = SquashPartitioner.getPartition(lookup, bw, Integer.MAX_VALUE)
-    a !=== b
+    val bw = Writables.bytesWritable(4096)
+    def getPartition(e: String): Int = {
+      SquashWritable.KeyState.set(f.withEntity(e), bw, fid)
+      SquashPartitioner.getPartition(lookup, bw, Integer.MAX_VALUE)
+    }
+    getPartition(e1) !=== getPartition(e2)
   })
+
+  // Make sure we don't try to compare the partition for two entities with the same hash (different from String.hashCode)
+  def hashEntry(s: String): Int = {
+    val bw = Writables.bytesWritable(s.length * 4)
+    val offset = ByteWriter.writeStringUTF8(bw.getBytes, s, 0)
+    bw.setSize(offset)
+    SquashWritable.GroupingByFeatureId.hashEntity(bw)
+  }
 }
