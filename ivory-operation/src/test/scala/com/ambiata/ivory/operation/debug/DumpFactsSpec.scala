28c52d641cdcac9f24c84a41b2f35a4115ca51c6
Charles O'Farrell <charleso@charleso.org>
2014-11-07 23:45:31 +1100

    Remove duplicate entities from DumpFactsSpec

--- ivory-operation/src/test/scala/com/ambiata/ivory/operation/debug/DumpFactsSpec.scala
+++ ivory-operation/src/test/scala/com/ambiata/ivory/operation/debug/DumpFactsSpec.scala
@@ -1,6 +1,6 @@
 package com.ambiata.ivory.operation.debug
 
-import org.specs2._, execute.{Failure => SpecsFailure}
+import org.specs2._
 import com.ambiata.mundane.testing.ResultTIOMatcher._
 import com.ambiata.notion.core.{TemporaryType => TT}
 import com.ambiata.ivory.core.{TemporaryLocations => T, _}
@@ -8,7 +8,6 @@ import com.ambiata.ivory.core.arbitraries._
 
 import com.ambiata.ivory.operation.extraction.Snapshots
 import com.ambiata.ivory.storage.repository.RepositoryBuilder
-import org.joda.time.LocalDate
 
 class DumpFactsSpec extends Specification with ScalaCheck { def is = s2"""
 
@@ -29,26 +28,32 @@ class DumpFactsSpec extends Specification with ScalaCheck { def is = s2"""
   }).set(minTestsOk = 3)
 
   def snapshots = prop((data: FactsWithDictionary) => {
+    val facts = uniqueEntities(data.facts)
     T.withIvoryLocationDir(TT.Hdfs) { location =>
       RepositoryBuilder.using { repository => for {
-        _ <- RepositoryBuilder.createRepo(repository, data.dictionary, List(data.facts))
+        _ <- RepositoryBuilder.createRepo(repository, data.dictionary.removeVirtualFeatures, List(data.facts))
         _ <- Snapshots.takeSnapshot(repository, Date.maxValue)
         _ <- DumpFacts.dump(repository, DumpFactsRequest(Nil, SnapshotId.initial :: Nil, Nil, Nil), location)
         r <- IvoryLocation.readLines(location)
       } yield r }
     } must beOkLike(r => {
-      (r.length, r.forall(_.endsWith(s"Snapshot[${SnapshotId.initial.render}]")), data.facts.forall(f => r.exists(l => l.contains(f.entity) && l.contains(f.feature)))) ==== ((data.facts.length, true, true)) })
+      (r.length, r.forall(_.endsWith(s"Snapshot[${SnapshotId.initial.render}]")), data.facts.forall(f => r.exists(l => l.contains(f.entity) && l.contains(f.feature)))) ==== ((facts.length, true, true)) })
   }).set(minTestsOk = 3)
 
   def both = propNoShrink((data: FactsWithDictionary) => {
+    val facts = uniqueEntities(data.facts)
     T.withIvoryLocationDir(TT.Hdfs) { location =>
       RepositoryBuilder.using { repository => for {
-        _ <- RepositoryBuilder.createRepo(repository, data.dictionary, List(data.facts))
+        _ <- RepositoryBuilder.createRepo(repository, data.dictionary.removeVirtualFeatures, List(facts))
         _ <- Snapshots.takeSnapshot(repository, Date.maxValue)
         _ <- DumpFacts.dump(repository, DumpFactsRequest(FactsetId.initial :: Nil, SnapshotId.initial :: Nil, Nil, Nil), location)
         r <- IvoryLocation.readLines(location)
       } yield r}
     } must beOkLike(r =>
-      (r.length, r.filter(_.endsWith(s"Snapshot[${SnapshotId.initial.render}]")).size, r.filter(_.endsWith(s"Factset[${FactsetId.initial.render}]")).size) ==== ((data.facts.length * 2, data.facts.length, data.facts.length)))
+      (r.length, r.filter(_.endsWith(s"Snapshot[${SnapshotId.initial.render}]")).size, r.filter(_.endsWith(s"Factset[${FactsetId.initial.render}]")).size) ==== ((facts.length * 2, facts.length, facts.length)))
   }).set(minTestsOk = 3)
+
+  // We're not here to test the snapshot logic, which involves complex windowing and priority
+  def uniqueEntities(facts: List[Fact]): List[Fact] =
+    facts.groupBy(_.entity).values.toList.flatMap(_.headOption)
 }
