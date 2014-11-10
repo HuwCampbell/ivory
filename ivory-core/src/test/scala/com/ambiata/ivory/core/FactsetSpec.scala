586fbb8080f0b29ab7726b330777322341d0788c
markhibberd <mark@hibberd.id.au>
2014-11-10 14:22:22 +1100

    Factset should have tests.

---
+++ ivory-core/src/test/scala/com/ambiata/ivory/core/FactsetSpec.scala
@@ -0,0 +1,32 @@
+package com.ambiata.ivory.core
+
+import org.specs2._
+import com.ambiata.ivory.core.arbitraries.Arbitraries._
+
+class FactsetSpec extends Specification with ScalaCheck { def is = s2"""
+
+Combinators
+-----------
+
+  filterByPartition is just a convenience on the underlying list of partitions.
+
+    ${ prop((f: Factset, p: Partition) =>
+         f.filterByPartition(_ == p).partitions ==== f.partitions.filter(_ == p)) }
+
+  filterByDate is just a convenience on the underlying list of partition dates.
+
+    ${ prop((f: Factset, d: Date) =>
+         f.filterByDate(_ > d).partitions ==== f.partitions.filter(_.date > d)) }
+
+  basic rules for filtering, i.e. const true is identity, const false is empty:
+
+    ${ prop((f: Factset) => f.filterByPartition(_ => true) ==== f) }
+
+    ${ prop((f: Factset) => f.filterByDate(_ => true) ==== f) }
+
+    ${ prop((f: Factset) => f.filterByPartition(_ => false).partitions ==== Nil) }
+
+    ${ prop((f: Factset) => f.filterByDate(_ => false).partitions ==== Nil) }
+
+"""
+}
