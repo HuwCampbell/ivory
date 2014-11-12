4e43cb1e727692912d86ea4177463f4ca31eeea0
Charles O'Farrell <charleso@charleso.org>
2014-11-12 12:48:48 +1100

    Remove extra IntervalReducer specs - not adding any value and flakey

--- ivory-operation/src/test/scala/com/ambiata/ivory/operation/extraction/reduction/IntervalReducerSpec.scala
+++ ivory-operation/src/test/scala/com/ambiata/ivory/operation/extraction/reduction/IntervalReducerSpec.scala
@@ -6,8 +6,6 @@ import org.specs2.{ScalaCheck, Specification}
 
 class IntervalReducerSpec extends Specification with ScalaCheck { def is = s2"""
   Interval mean reducer works with arbitrary facts       $meanInterval
-  Interval sd reducer works with arbitrary facts         $sdInterval
-  Interval grad reducer works with arbitrary facts       $gradInterval
 
 """
 
@@ -25,34 +23,4 @@ class IntervalReducerSpec extends Specification with ScalaCheck { def is = s2"""
       else 0.0
     r.save.getD must beCloseTo(res, 4.significantFigures)
   })
-
-  def sdInterval = prop((facts: List[Fact]) => {
-    val ds = facts.map(td => 0 -> td.date).sortBy(_._2)
-    val dateOffsets = ReducerUtil.buildDateOffsets(ds)
-
-    val r = new IntervalReducer(dateOffsets, new StandardDeviationReducer, ReductionValueDouble)
-    facts.sortBy(_.date).foreach(r.update)
-
-    val filteredfacts = facts.filterNot(_.isTombstone).sortBy(_.date)
-    val res =
-      if (filteredfacts.length > 2)
-        ReducerMathsHelpers.stdDev(filteredfacts.sliding(2, 1).collect { case a :: b :: Nil => DateTimeUtil.toDays(b.date) - DateTimeUtil.toDays(a.date) }.toList)
-      else 0.0
-    r.save.getD must beCloseTo(res, 4.significantFigures)
-  })
-
-  def gradInterval = prop((facts: List[Fact]) => {
-    val ds = facts.map(td => 0 -> td.date).sortBy(_._2)
-    val dateOffsets = ReducerUtil.buildDateOffsets(ds)
-
-    val r = new IntervalReducer(dateOffsets, new GradientReducer(dateOffsets), ReductionValueDouble)
-    facts.sortBy(_.date).foreach(r.update)
-
-    val filteredfacts = facts.filterNot(_.isTombstone).sortBy(_.date)
-    val res = if (filteredfacts.length > 2)
-        ReducerMathsHelpers.gradient(filteredfacts.sliding(2, 1).collect { case a :: b :: Nil => (DateTimeUtil.toDays(b.date) - DateTimeUtil.toDays(a.date), b.date) }.toList)
-      else 0.0
-    r.save.getD must beCloseTo(res, 4.significantFigures)
-  })
-
 }
