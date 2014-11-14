5a7a7c1f7f84bde9a79f1a4766a8acb8b39acef3
Nick Hibberd <nhibberd@gmail.com>
2014-11-14 11:39:06 +1100

    Adding resolver

--- project/depend.scala
+++ project/depend.scala
@@ -84,5 +84,6 @@ object depend {
       "cloudera"             at "https://repository.cloudera.com/content/repositories/releases",
       "cloudera2"            at "https://repository.cloudera.com/artifactory/public",
       Resolver.url("ambiata-oss", new URL("https://ambiata-oss.s3.amazonaws.com"))(Resolver.ivyStylePatterns),
-      "Scalaz Bintray Repo"  at "http://dl.bintray.com/scalaz/releases")
+      "Scalaz Bintray Repo"  at "http://dl.bintray.com/scalaz/releases",
+      "spray.io"             at "http://repo.spray.io")
 }
