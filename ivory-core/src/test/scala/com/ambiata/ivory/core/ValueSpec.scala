aedcc7ce505c20dbb810b8e8b86e6f4e95939008
Russell Aronson <russell.aronson@gmail.com>
2014-12-16 12:57:32 +1100

    Add validation check on entity length up to 256

--- ivory-core/src/test/scala/com/ambiata/ivory/core/ValueSpec.scala
+++ ivory-core/src/test/scala/com/ambiata/ivory/core/ValueSpec.scala
@@ -1,6 +1,8 @@
 package com.ambiata.ivory.core
 
+import org.scalacheck._, Arbitrary._
 import com.ambiata.ivory.core.arbitraries._, Arbitraries._
+import com.ambiata.ivory.core.gen._
 import org.specs2.{ScalaCheck, Specification}
 
 class ValueSpec extends Specification with ScalaCheck { def is = s2"""
@@ -8,6 +10,7 @@ class ValueSpec extends Specification with ScalaCheck { def is = s2"""
   Can validate with correct encoding                     $valid
   Can validate with incorrect encoding                   $invalid
   Can convert to and from thrift                         $thrift
+  Facts with entity ids larger then 256 are invalid      $invalidEntityIdFact
 """
 
   def valid = prop((e: EncodingAndValue) =>
@@ -22,6 +25,9 @@ class ValueSpec extends Specification with ScalaCheck { def is = s2"""
     Value.fromThrift(Value.toThrift(v)) ==== v
   }
 
+  def invalidEntityIdFact = prop((fact: BadEntityFact) =>
+    Value.validateFact(fact.fact, fact.dictionary).toEither must beLeft)
+
   // A small subset of  encoded values are valid for different optional/empty Structs/Lists
   private def isCompatible(e1: EncodingAndValue, e2: Encoding): Boolean =
     (e1, e2) match {
@@ -29,4 +35,13 @@ class ValueSpec extends Specification with ScalaCheck { def is = s2"""
       case (EncodingAndValue(_, ListValue(l)), ListEncoding(e))     => l.forall(v => isCompatible(EncodingAndValue(e, v), e))
       case _ => false
     }
+
+  case class BadEntityFact(fact: Fact, dictionary: Dictionary)
+  implicit def BadEntityArbitrary: Arbitrary[BadEntityFact] = Arbitrary(for {
+    n     <- Gen.choose(257, 1000)
+    chars <- Gen.listOfN(n, arbitrary[Char])
+    cg    <- arbitrary[ConcreteGroupFeature]
+    dt    <- arbitrary[DateTime]
+    fact  <- GenFact.factWith(chars.mkString, cg.fid, cg.cg.definition, dt)
+  } yield BadEntityFact(fact, cg.dictionary))
 }
