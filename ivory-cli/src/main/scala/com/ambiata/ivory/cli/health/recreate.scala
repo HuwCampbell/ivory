694bb3407c597e6eaf639f1f5458fd51fe99aa76
Russell Aronson <russell.aronson@gmail.com>
2015-01-06 18:08:06 +1100

    Cleanup recreate cli

--- ivory-cli/src/main/scala/com/ambiata/ivory/cli/health/recreate.scala
+++ ivory-cli/src/main/scala/com/ambiata/ivory/cli/health/recreate.scala
@@ -1,29 +1,25 @@
 package com.ambiata.ivory.cli.health
 
-import com.ambiata.ivory.cli.{IvoryCmd, IvoryRunner, IvoryApp}
+import com.ambiata.ivory.cli._, ScoptReaders._
 import com.ambiata.ivory.core._
 import com.ambiata.ivory.storage.repository._
-import com.ambiata.ivory.storage.control._
-import com.ambiata.mundane.control._
 
-import scalaz._, Scalaz._, effect.IO
+import scalaz._, effect.IO
 
 object recreate extends IvoryApp {
-  case class CliArguments(repository: String, factsets: List[String], reducerSize: Option[Long])
+  case class CliArguments(factsets: List[FactsetId])
 
   val parser = new scopt.OptionParser[CliArguments]("health-recreate") {
     head("""Recreate factsets using the latest format/compression/block size.""")
 
     help("help") text "shows this usage text"
 
-    opt[String]('r', "repository") action { (x, c) => c.copy(repository = x) }             required() text "Ivory repository."
-    opt[String]('f', "factset")    action { (x, c) => c.copy(factsets = x :: c.factsets) } required() text "Id of factset to recreate."
+    opt[FactsetId]('f', "factset") unbounded() optional() action {
+      (x, c) => c.copy(factsets = x :: c.factsets)
+    }
   }
 
-  val cmd = IvoryCmd[CliArguments](parser, CliArguments(repository = "", factsets = Nil, reducerSize = None),
-    IvoryRunner(configuration => c => for {
-      repo      <- IvoryT.fromResultTIO(HdfsRepository.fromUri(c.repository, configuration))
-      fids      <- c.factsets.traverseU(f => IvoryT.fromResultTIO(ResultT.fromOption(FactsetId.parse(f), s"Invalid factset id '${f}'!")))
-      recreated <- RecreateFactset.recreateFactsets(repo, fids)
-    } yield List(recreated.successString)))
+  val cmd = IvoryCmd.withRepo[CliArguments](parser, CliArguments(factsets = Nil), { repo => conf => c =>
+    RecreateFactset.recreateFactsets(repo, c.factsets).map(re => List(re.successString))
+  })
 }
