fb3f80f0b631a91a9cfe8edb78f0c2514ee5ad19
markhibberd <mark@hibberd.id.au>
2015-01-07 13:36:53 +1100

    Added back edge cases spec in the correct WindowSpec spot.

--- ivory-core/src/test/scala/com/ambiata/ivory/core/WindowSpec.scala
+++ ivory-core/src/test/scala/com/ambiata/ivory/core/WindowSpec.scala
@@ -6,6 +6,8 @@ import com.ambiata.ivory.core.arbitraries.Arbitraries._
 class WindowSpec extends Specification with ScalaCheck { def is = s2"""
 
   Can calculate the date for a window                        $dateLookup
+  Starting date edge cases                                   $startingDateEdgeCases
+
 """
 
   def dateLookup = prop { (w: Window, d: Date) =>
@@ -21,4 +23,19 @@ class WindowSpec extends Specification with ScalaCheck { def is = s2"""
       case Years  => local.minusYears _
     })(window.length))
   }
+
+  // It's a little hard to write general property tests for this
+  def startingDateEdgeCases =
+    seqToResult(List(
+      (Window(1, Days),   Date(2012, 3, 1),  Date(2012, 2, 29), "day: leap year"),
+      (Window(1, Weeks),  Date(2012, 3, 7),  Date(2012, 2, 29), "week: leap year"),
+      (Window(1, Months), Date(2011, 3, 30), Date(2011, 2, 28), "month: non leap year"),
+      (Window(1, Months), Date(2012, 3, 30), Date(2012, 2, 29), "month: leap year"),
+      (Window(1, Years),  Date(2012, 2, 29), Date(2011, 2, 28), "year: from leap year"),
+      (Window(1, Years),  Date(2013, 2, 28), Date(2012, 2, 28), "year: to leap year"),
+      (Window(1, Years),  Date(2014, 2, 28), Date(2013, 2, 28), "year: not leap year")
+    ).map {
+      case (window, start, e, reason) => (Window.startingDate(window, start) ==== e).setMessage(reason)
+    })
+
 }
