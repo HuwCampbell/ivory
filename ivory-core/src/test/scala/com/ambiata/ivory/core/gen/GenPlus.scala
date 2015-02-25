2c9897f491cb22e85840b4ccbbe3d0fbbd4c690b
Charles O'Farrell <charleso@charleso.org>
2015-02-25 13:13:56 +1100

    Introduced GenPlus.listOfSizedWithIndex

--- ivory-core/src/test/scala/com/ambiata/ivory/core/gen/GenPlus.scala
+++ ivory-core/src/test/scala/com/ambiata/ivory/core/gen/GenPlus.scala
@@ -1,7 +1,7 @@
 package com.ambiata.ivory.core.gen
 
 import org.scalacheck.Gen, Gen.Choose
-
+import scalaz._, Scalaz._, scalaz.scalacheck.ScalaCheckBinding._
 
 // FIX ARB move to disorder.
 object GenPlus {
@@ -13,6 +13,13 @@ object GenPlus {
   def posNum[A](implicit num: Numeric[A], c: Choose[A]): Gen[A] =
     Gen.sized(max => c.choose(num.one, num.max(num.one, num.fromInt(max))))
 
+  def sizedInt(from: Int, to: Int): Gen[Int] =
+    Gen.sized(n => Gen.choose(from, Math.max(from, Math.min(n, to))))
+
   def listOfSized[A](from: Int, to: Int, gen: => Gen[A]): Gen[List[A]] =
-    Gen.sized(n => Gen.choose(from, Math.max(from, Math.min(n, to))).flatMap(i => Gen.listOfN(i, gen)))
+    sizedInt(from, to).flatMap(i => Gen.listOfN(i, gen))
+
+  /** Useful for tagging a Gen value with a unique int */
+  def listOfSizedWithIndex[A](from: Int, to: Int, gen: Int => Gen[A]): Gen[List[A]] =
+    sizedInt(from, to).flatMap(i => (0 until i).toList.traverse(gen))
 }
