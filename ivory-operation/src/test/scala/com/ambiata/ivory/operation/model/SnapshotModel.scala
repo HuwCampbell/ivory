1172517e81b3c791e109afd39d37fd4359d34c3e
Charles O'Farrell <charleso@charleso.org>
2015-03-11 08:24:29 +1100

    Add keyed value to snapshot model ordering

--- ivory-operation/src/test/scala/com/ambiata/ivory/operation/model/SnapshotModel.scala
+++ ivory-operation/src/test/scala/com/ambiata/ivory/operation/model/SnapshotModel.scala
@@ -10,21 +10,33 @@ case class SnapshotModelConf(date: Date, dictionary: Dictionary)
 object SnapshotModel extends MapReduceSimple[Prioritized[Fact], Fact, SnapshotModelConf] {
   type M = Fact
   type K = (String, FeatureId)
-  type S = (DateTime, Priority)
+  type S = (DateTime, Option[PrimitiveValue], Priority)
 
   def map(facts: List[Prioritized[Fact]], conf: SnapshotModelConf): List[(K, S, Fact)] =
     facts
       .filter(_.value.date <= conf.date)
-      .map(mapKeyed)
+      .map(pf => mapKeyedDict(conf.dictionary, pf))
 
-  def mapKeyed(pf: Prioritized[Fact]): (K, S, Fact) = {
+  def mapKeyedDict(dict: Dictionary, pf: Prioritized[Fact]): (K, S, Fact) =
+    mapKeyed(dict.byConcrete.sources.getOrElse(pf.value.featureId, sys.error(s"Unknown feature for $pf")).definition, pf)
+
+  def mapKeyed(cd: ConcreteDefinition, pf: Prioritized[Fact]): (K, S, Fact) = {
     val f = pf.value
-    ((f.entity, f.featureId), (f.datetime, pf.priority), f)
+    val v = cd.mode.fold(None, None, key => f.value match {
+      case StructValue(m) =>
+        m.get(key)
+      case _ =>
+        sys.error(s"Non-struct value for keyed_set fact: $f")
+    })
+    ((f.entity, f.featureId), (f.datetime, v, pf.priority), f)
   }
 
   def secondary: Order[S] =
-    Order.orderBy {
-      case (dt, p) => (dt.long, p)
+    Order.order {
+      case ((dt1, v1, p1), (dt2, v2, p2)) =>
+        dt1.long ?|? dt2.long |+|
+          optionOrder(Value.orderPrimitive)(v1, v2) |+|
+          p1 ?|? p2
     }
 
   def reduce(key: K, facts: NonEmptyList[Fact], conf: SnapshotModelConf): List[Fact] =
